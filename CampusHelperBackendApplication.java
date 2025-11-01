2134fc07872de212748df416caf9f69b6b391496
GithuSam776688 <ry526270@gmail.com>
2025-11-02 01:49:03 +0530

    Add files via upload

---
+++ CampusHelperBackendApplication.java
@@ -0,0 +1,13 @@
+package com.collegehelper.campus_helper_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampusHelperBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampusHelperBackendApplication.class, args);
+	}
+
+}
---
+++ CampusHelperBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.collegehelper.campus_helper_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampusHelperBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
